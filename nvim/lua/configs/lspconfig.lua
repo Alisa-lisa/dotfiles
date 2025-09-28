8a2c0bbdc2cae2205237d7634a742e6d771a3c21
alisa.mindpeak <alisa.dammer@gmail.com>
2025-09-28 21:19:29 +0200

    update nvim lsp

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -2,7 +2,8 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local nvlsp = require "nvchad.configs.lspconfig"
-local lspconfig = require "lspconfig"
+nvlsp.defaults()
+-- local lspconfig = require "lspconfig"
 
 -- EXAMPLE
 local servers = {
@@ -15,14 +16,38 @@ local servers = {
   "taplo",
 }
 
+local overrides = {
+  lua_ls = {
+    settings = {
+      Lua = {
+        diagnostics = { globals = { "vim" } },
+        workspace = { checkThirdParty = false },
+      },
+    },
+  },
+  ruff = {
+    init_options = { settings = { args = {} } }, -- add ruff args here if you want
+  },
+}
 -- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+for _, name in ipairs(servers) do
+  vim.lsp.config(
+    name,
+    vim.tbl_deep_extend("force", {
+      on_attach = nvlsp.on_attach,
+      capabilities = nvlsp.capabilities,
+    }, overrides[name] or {})
+  )
+
+  vim.lsp.enable(name)
 end
+-- for _, lsp in ipairs(servers) do
+--   lspconfig[lsp].setup {
+--     on_attach = nvlsp.on_attach,
+--     on_init = nvlsp.on_init,
+--     capabilities = nvlsp.capabilities,
+--   }
+-- end
 
 -- toml
 -- lspconfig.taplo.setup {}
